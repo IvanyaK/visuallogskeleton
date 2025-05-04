f7bbe8727a9757ac0c71e37a5634e9250c8dcbc1
IvanyaK <ivanrus200519@gmail.com>
2025-05-04 14:10:06 +0300

    resolving problem with date_time and column name distiction

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ from typing import Any, Dict
 
 from chardet import detect
 from pandas import DataFrame, read_csv
-from pm4py import discover_log_skeleton, read_xes
+from pm4py import discover_log_skeleton, format_dataframe, read_xes
 
 
 # Determines file format and retrieves event logs
 def getting_log(file_path: str) -> DataFrame:
     if file_path.split(".")[-1] == "xes":
         return read_xes(file_path)
+
     if file_path.split(".")[-1] == "csv":
         with open(file_path, "rb") as f:
             result = detect(f.read())
+
         log = read_csv(file_path, encoding=result["encoding"])
+
+        case_id = input("Your case_id: ")
+        activity_key = input("Your activity_key: ")
+        timestamp_key = input("Your timestamp: ")
+        log = format_dataframe(
+            log,
+            case_id=case_id,
+            activity_key=activity_key,
+            timestamp_key=timestamp_key,
+            timest_format="%Y-%m-%d %H:%M:%S",
+        )
+
         return log
 
 
-# TODO: when import csv - exeption: the dataframe should (at least) contain a column of type date
 def build_log_skeleton(log: DataFrame) -> Dict[str, Any]:
     log_skeleton = discover_log_skeleton(log, noise_threshold=0.0)
     return log_skeleton
