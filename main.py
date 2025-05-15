b87f1d8f5bd399f1683e12aca7718758d8a415b4
IvanyaK <ivanrus200519@gmail.com>
2025-05-15 12:23:46 +0300

    added try-except for format_dataframe and strip spaces in file path

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ from typing import Any, Dict
 
 from chardet import detect
 from pandas import DataFrame, read_csv
-
 from pm4py import (
     discover_log_skeleton,
     format_dataframe,
     read_xes,
 )
-
 from pm4py.analysis import insert_artificial_start_end
 
 from visualize import visualize_model
 
 
 def getting_log(file_path: str) -> DataFrame:
-
     if file_path.lower().endswith(".xes"):
         log_df = read_xes(file_path)
 
@@ -24,18 +21,21 @@ def getting_log(file_path: str) -> DataFrame:
             result = detect(f.read())
 
         log_df = read_csv(file_path, encoding=result["encoding"])
-
-        case_id = input("Your case_id column: ")
-        activity_key = input("Your activity_key column: ")
-        timestamp_key = input("Your timestamp column: ")
-
-        log_df = format_dataframe(
-            log_df,
-            case_id=case_id,
-            activity_key=activity_key,
-            timestamp_key=timestamp_key,
-            timest_format="%Y-%m-%d %H:%M:%S",
-        )
+        while True:
+            case_id = input("Your case_id column: ")
+            activity_key = input("Your activity_key column: ")
+            timestamp_key = input("Your timestamp column: ")
+            try:
+                log_df = format_dataframe(
+                    log_df,
+                    case_id=case_id,
+                    activity_key=activity_key,
+                    timestamp_key=timestamp_key,
+                    timest_format="%Y-%m-%d %H:%M:%S",
+                )
+                break
+            except Exception:
+                print("Your values is not in the dataframe!")
     else:
         raise ValueError("Supported extensions: .xes or .csv")
 
@@ -49,13 +49,14 @@ def build_log_skeleton(log: DataFrame, noise: float = 0.9) -> Dict[str, Any]:
 
 
 def main() -> None:
-    path = input('Enter path to .xes / .csv file: ').strip('"')
+    path = input("Enter path to .xes / .csv file: ").strip('"').strip()
     log = getting_log(path)
 
     log_skeleton = build_log_skeleton(log)
-    #print(log_skeleton)    # Отладка       
+    # print(log_skeleton)    # Отладка
 
     visualize_model(log_skeleton)
+
 
 if __name__ == "__main__":
     main()
--- visualize.py
+++ visualize.py
@@ -1,22 +1,25 @@
 from graphviz import Digraph
 
+
 def visualize_model(model):
     always_after = model.get("always_after", [])
     directly_follows = model.get("directly_follows", [])
 
     dot = Digraph("LogSkeleton", format="png", engine="dot")
     dot.attr(rankdir="TB", splines="polyline", bgcolor="white")
-    dot.attr("node",
-             shape="Mrecord",
-             style="filled",
-             fontname="Times-New-Roman",
-             fontsize="14")
+    dot.attr(
+        "node",
+        shape="Mrecord",
+        style="filled",
+        fontname="Times-New-Roman",
+        fontsize="14",
+    )
 
     for act, counts in model["activ_freq"].items():
         lo, hi = min(counts), max(counts)
         interval = f"{lo}..{hi}" if lo != hi else str(hi)
         label = f"{{ {act} | {{ Name | {sum(counts)} | {interval} }} }}"
-        dot.node(act, label=label, fillcolor='antiquewhite')
+        dot.node(act, label=label, fillcolor="antiquewhite")
 
     for a, b in directly_follows:
         dot.edge(a, b, color="sienna", penwidth="2")
@@ -24,4 +27,4 @@ def visualize_model(model):
     for a, b in always_after:
         dot.edge(a, b, color="#4daf4a", style="dashed")
 
-    dot.render('log_skeleton_graph', view=True, cleanup=True)
+    dot.render("log_skeleton_graph", view=True, cleanup=True)
